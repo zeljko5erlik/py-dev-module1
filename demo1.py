7922f2dc6e2d23d7be40853960157f4b159bfd1e
zeljko5erlik <zeljko.peterlik@gmail.com>
2023-11-21 19:46:49 +0000

    added demo files

---
+++ demo1.py
@@ -0,0 +1,10 @@
+print('Dobro dosli u PyDev program obrazovanja')
+
+first_name = 'Pero'
+last_name = 'Perić'
+date_of_birth = 1998
+mobile_phone = '+385 9n 1234 567'
+employed = True
+height = 180.99
+
+print(first_name, last_name)
